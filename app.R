0c85b131145fc5745488514b819478f078ed2b71
Herveen Singh <herveenksingh@gmail.com>
2021-07-08 14:30:06 -0400

    Herveen

--- app.R
+++ app.R
@@ -9,6 +9,9 @@
 
 library(shiny)
 library(googlesheets4)
+hervFinance <- read_sheet("https://docs.google.com/spreadsheets/d/1ThNMemTz_HrpkVQSWEy3SFu0TdMn4OZDu0s9NGI5fNQ/edit#gid=0")
+hervFinance
+
 source("ui.R")
 
 #get ui from the ui.R file
